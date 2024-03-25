4b3e75da7714d3aff413bc0304032ac6f9b2aaa7
Carlos Araujo <carlosaraujo@Carloss-MBP.attlocal.net>
2024-03-24 19:52:16 -0500

    upload2

--- Starter_Code/starter_code/api_keys.py
+++ Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "f3b29d54b08bfce170f92c367448ecce"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "537d8d2e60b34c5b84c6bbce83a0a0df"
